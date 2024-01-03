7e87d794be657f9abb41354ae81b7c4319c95fa8
vikashPirth <vikash.pirthiani@gmail.com>
2024-01-03 15:08:29 +0500

    Created Room class

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class GameObject:
   
     # Initializing the fields of the class
@@ -6,7 +8,7 @@ class GameObject:
         self.appearance = appearance
         self.feel = feel
         self.smell = smell
-        
+
     # Implementing the methods of our class
     def look(self):
         return f"You look at the {self.name}. {self.appearance}\n"
@@ -17,3 +19,16 @@ class GameObject:
     def sniff(self):
         return f"You sniff the {self.name}. {self.smell}\n"
 
+
+class Room:
+
+    def __init__(self, escape_code: int, game_objects: List[GameObject]) -> None:
+        self.escape_code = escape_code
+        self.game_objects= game_objects
+    
+    def check_code(self, code):
+        return self.escape_code == code
+    
+    def get_game_object_names(self):
+        return [game_object.name for game_object in self.game_objects]
+     
