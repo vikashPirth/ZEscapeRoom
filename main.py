0d20fa69ba9b45c2d4e225ed75a9cf590d2108b9
vikashPirth <vikash.pirthiani@gmail.com>
2024-01-03 15:25:46 +0500

    created Room class

--- main.py
+++ main.py
@@ -31,4 +31,40 @@ class Room:
     
     def get_game_object_names(self):
         return [game_object.name for game_object in self.game_objects]
-     
+    
+
+class Game:
+
+    def __init__(self) -> None:
+        self.attempts = 0
+        objects = self.create_objects()
+        self.room = Room(731, objects)
+    
+    def create_objects(self):
+        return [
+          GameObject(
+            "Sweater",
+            "It's a blue sweater that had the number 12 switched on it.",
+            "Someone has unstitched the second number, leaving only the 1.",
+            "The sweater smells of laundry detergent."),
+          GameObject(
+            "Chair", 
+            "It's a wooden chair with only 3 legs.",
+            "Someone had deliberately snapped off one of the legs.",
+            "It smells like old wood."),
+          GameObject(
+            "Journal",
+            "The final entry states that time should be hours then minutes then seconds (H-M-S).",
+            "The cover is worn and several pages are missing.",
+            "It smells like musty leather."),
+          GameObject(
+            "Bowl of soup", 
+            "It appears to be tomato soup.",
+            "It has cooled down to room temperature.",
+            "You detect 7 different herbs and spices."),
+          GameObject(
+            "Clock", 
+            "The hour hand is pointing towards the soup, the minute hand towards the chair, and the second hand towards the sweater.",
+            "The battery compartment is open and empty.",
+            "It smells of plastic."),
+        ]
